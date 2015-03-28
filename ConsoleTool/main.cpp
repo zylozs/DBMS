07e50608bc50dcd21edf5e5ab5571bbf53e48595
Vincent <vincent.loignon@gmail.com>
2015-03-28 14:15:11 -0400

    - Fixed a potential problem in 3NF with finding the minimal basis - Created the base input system for the program

--- ConsoleTool/main.cpp
+++ ConsoleTool/main.cpp
@@ -1,29 +1,234 @@
 #include "main.h"
 
-#include "ChaseTest.h"
-#include "ThirdNF.h"
-#include "Relation.h"
-#include "FD.h"
-#include "Utils.h"
-
 int main()
 {
-	printf("Welcome to Kyle & Vince's DBMS Console Tool!\n");
+	printf("--------------------------------------------------------------------------------\n");
+	printf("||               Welcome to Kyle & Vince's DBMS Console Tool!                 ||\n");
+	printf("--------------------------------------------------------------------------------\n");
 
-	ThirdNF thirdNF;
-	Relation origRel("ABCD");
-	std::vector<Relation> newRels;
-	std::vector<FD> fds;
+	while (true)
+	{
+		ThirdNF thirdNF;
+		Relation origRel;
+		std::vector<Relation> newRels;
+		std::vector<FD> fds;
+		int option = 0;
+		int outputOption = 0;
+		std::string outputFile = "";
 
-	newRels.push_back(Relation("BC"));
-	newRels.push_back(Relation("BD"));
-	newRels.push_back(Relation("AB"));
+		while (true)
+		{
+			printf("What would you like?\n");
+			printf("1. 3NF\n");
+			printf("2. Exit\n");
+			printf("> ");
 
-	fds.push_back(FD("B", "C"));
-	fds.push_back(FD("B", "D"));
+			std::string temp = "";
+			getline(std::cin, temp);
 
-	thirdNF.normalize(origRel, fds);
+			option = Utils::convertToInt<std::string>(temp);
+			
+			if (option != 1 && option != 2)
+			{
+				printf("\nInvalid Input.\n\n");
+				continue;;
+			}
+
+			break;
+		}
+
+		if (option == 1)
+		{
+			// Get relation
+			while (true)
+			{
+				printf("\nEnter your Relation. White space will be ignored.\n");
+				printf("Usage: R(A, B, ...) \n\n");
+				printf("> ");
+
+				// Grab their input
+				std::string temp = "";
+				getline(std::cin, temp);
+
+				// extract the relation
+				if (!extractRelation(temp, origRel))
+				{
+					printf("\nInvalid Usage.\n\n");
+					continue;
+				}
+
+				break;
+			}
+
+			// Get FDs
+			while (true)
+			{
+				printf("\nEnter your Functional Dependencies. White sapce will be ignored.\n");
+				printf("Usage: B->C, AD->E, ...\n");
+				printf("> ");
+
+				std::string temp = "";
+				getline(std::cin, temp);
+
+				if (!extractFDs(temp, fds))
+				{
+					fds.clear();
+
+					printf("\nInvalid Usage.\n\n");
+					continue;;
+				}
+
+				break;
+			}
+
+			// Get output file
+			while(true)
+			{
+				printf("\nWhere would you like it to output? (You can specify the file)\n");
+				printf("Usage: \tconsole\n\tfile.txt\n\tboth file.txt\n");
+				printf("> ");
+
+				std::string temp = "";
+				getline(std::cin, temp);
+
+				// Just console
+				if (temp == "console")
+					outputOption = 1;
+				else
+				{
+					size_t i = temp.find("both ");
+
+					// Just file
+					if (i == std::string::npos)
+					{
+						outputOption = 2;
+						outputFile = temp;
+					}
+					else // File and console
+					{
+						outputOption = 3;
+
+						outputFile = temp.substr(5);
+					}
+				}
+				
+				break;
+			}
+
+			thirdNF.normalize(origRel, fds);
+		}
+		else if (option == 2)
+		{
+			break;
+		}
+
+		system("pause");
+	}
 
 	system("pause");
 	return 0;
+}
+
+bool extractRelation(std::string input, Relation& out)
+{
+	// Remove spaces to make it easier to parse
+	input = removeSpaces(input);
+
+	// Find the first (, everyting before this is the relation's name
+	size_t startIndex = input.find_first_of('(');
+	size_t finalIndex = input.find_first_of(')');
+
+	// If npos was returned, their format is wrong!
+	if (startIndex == std::string::npos || finalIndex == std::string::npos)
+		return false;
+
+	std::string relName = "";
+	std::string attributes = "";
+	size_t i = 0;
+
+	relName = input.substr(0, startIndex);
+	attributes = input.substr(startIndex + 1, finalIndex - 2);
+
+	// Split the attributes into individual attributes which we will then store in the relation
+	while (true)
+	{
+		i = attributes.find_first_of(',');
+
+		if (i == std::string::npos)
+		{
+			out += attributes;
+			break;
+		}
+
+		out += attributes.substr(0, i);
+
+		attributes = attributes.substr(i + 1);
+	}
+
+	return true;
+}
+
+bool extractFDs(std::string input, std::vector<FD>& out)
+{
+	// Remove spaces to make it easier to parse
+	input = removeSpaces(input);
+
+	std::string fd = "";
+	std::string left = "";
+	std::string right = "";
+	size_t i = 0;
+
+	// Split the attributes into individual attributes which we will then store in the relation
+	while (true)
+	{
+		i = input.find_first_of(',');
+
+		if (i == std::string::npos)
+		{
+			if (!extractFD(input, left, right))
+				return false;
+
+			out.push_back(FD(left, right));
+			break;
+		}
+
+		fd = input.substr(0, i);
+
+		if (!extractFD(fd, left, right))
+			return false;
+
+		out.push_back(FD(left, right));
+
+		input = input.substr(i + 1);
+	}
+
+	return true;
+}
+
+bool extractFD(std::string fd, std::string& left, std::string& right)
+{
+	size_t i = fd.find("->");
+
+	if (i == std::string::npos)
+		return false;
+
+	left = fd.substr(0, i);
+	right = fd.substr(i + 2);
+
+	return true;
+}
+
+std::string removeSpaces(std::string input)
+{
+	std::string output = "";
+
+	for (unsigned int i = 0; i < input.size(); i++)
+	{
+		if (input[i] == ' ')
+			continue;
+
+		output += input[i];
+	}
+
+	return output;
 }
--- ConsoleTool/main.h
+++ ConsoleTool/main.h
@@ -1,3 +1,13 @@
 #pragma once
 
 #include <iostream>
+#include "ChaseTest.h"
+#include "ThirdNF.h"
+#include "Relation.h"
+#include "FD.h"
+#include "Utils.h"
+
+bool extractRelation(std::string input, Relation& out);
+bool extractFDs(std::string input, std::vector<FD>& out);
+bool extractFD(std::string fd, std::string& left, std::string& right);
+std::string removeSpaces(std::string input);
